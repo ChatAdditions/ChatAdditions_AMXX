a546ae502e63083de6426221f6048eabb7275c92
SergeyShorokhov <wopox1337@ya.ru>
2021-02-24 02:31:55 +0300

    :art: Improve codestyle in publish.sh script

--- publish.sh
+++ publish.sh
@@ -1,77 +1,90 @@
-#!/bin/bash          
+#!/bin/bash
 
 ########################
-AMXX_SCRIPTING_PATH=""
+ # Output directory
 OUTPUT_DIR="_PUBLISH"
+ # Release asset name (<releaseName>.tar.gz)
 RELEASE_NAME="ChatAdditions_publish"
+ # AMXModX version. Should be 1.9 or 1.10
 AMXMODX="1.9"
 ########################
+
+ # Default compiler directory
 COMPILER_PATH=".compiler"
 
+# If a compiler folder not found - create new one
 if [ ! -d ${COMPILER_PATH} ]; then
-    TEMPDIR="./.temp"
+  TEMPDIR="./.temp"
 
-    mkdir ${TEMPDIR}
-    AMXX_FILENAME=$(curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/amxmodx-latest-base-linux)
-    curl -s https://www.amxmodx.org/amxxdrop/${AMXMODX}/${AMXX_FILENAME} | tar -xz -C ${TEMPDIR}
+  # Download AMXModX with linux and windows amxxpc binaries
+  mkdir ${TEMPDIR}
+  AMXX_FILENAME=$(curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/amxmodx-latest-base-linux)
+  curl -s https://www.amxmodx.org/amxxdrop/${AMXMODX}/${AMXX_FILENAME} | tar -xz -C ${TEMPDIR}
 
-    AMXX_FILENAME=$(curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/amxmodx-latest-base-windows)
-    curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/${AMXX_FILENAME} -o amxmodx.zip
-    unzip -o amxmodx.zip -d ${TEMPDIR}
-    rm amxmodx.zip
+  AMXX_FILENAME=$(curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/amxmodx-latest-base-windows)
+  curl https://www.amxmodx.org/amxxdrop/${AMXMODX}/${AMXX_FILENAME} -o amxmodx.zip
+  unzip -o amxmodx.zip -d ${TEMPDIR}
+  rm amxmodx.zip
 
-    curl -OL https://github.com/s1lentq/reapi/releases/download/5.19.0.211/reapi_5.19.0.211-dev.zip
-    unzip -o reapi_*-dev.zip -d ${TEMPDIR}
-    rm reapi_*-dev.zip
+  # Download ReAPI
+  curl -OL https://github.com/s1lentq/reapi/releases/download/5.19.0.211/reapi_5.19.0.211-dev.zip
+  unzip -o reapi_*-dev.zip -d ${TEMPDIR}
+  rm reapi_*-dev.zip
 
-    rm -rf ${TEMPDIR}/addons/amxmodx/scripting/testsuite/
-    rm -rf ${TEMPDIR}/addons/amxmodx/scripting/*.sma
-    rm -rf ${TEMPDIR}/addons/amxmodx/plugins/*.amxx
+  # Remove extra content which not used
+  rm -rf ${TEMPDIR}/addons/amxmodx/scripting/testsuite/
+  rm -rf ${TEMPDIR}/addons/amxmodx/scripting/*.sma
+  rm -rf ${TEMPDIR}/addons/amxmodx/plugins/*.amxx
 
-    mkdir -p ${COMPILER_PATH}
-    mv -f ${TEMPDIR}/addons/amxmodx/scripting/* ${COMPILER_PATH}
+  # Move compiler (AMXX + ReAPI) to permanent folder
+  mkdir -p ${COMPILER_PATH}
+  mv -f ${TEMPDIR}/addons/amxmodx/scripting/* ${COMPILER_PATH}
 
-    rm -rf ${TEMPDIR}
+  # Remove extra files
+  rm -rf ${TEMPDIR}
 
-    echo -e "\n=========================\n > Compiler ${AMXX_FILENAME} and ReAPI succefully downloaded!"
+  echo -e "\n=========================\n > Compiler ${AMXX_FILENAME} and ReAPI succefully downloaded!"
 else
-    echo -e "\n=========================\n > Compiler found at: ${COMPILER_PATH} path"
+  echo -e "\n=========================\n > Compiler found at: ${COMPILER_PATH} path"
 fi
 
 AMXX_SCRIPTING_PATH=${COMPILER_PATH}
 amxxpc="${AMXX_SCRIPTING_PATH}/amxxpc"
 
+# Determinate win32 and use own compiler
 case "$OSTYPE" in
-  msys*)    amxxpc="${amxxpc}.exe" ;;
-  *)        echo "unknown: $OSTYPE" ;;
+  msys*)  amxxpc="${amxxpc}.exe" ;;
+  *)      echo "unknown: $OSTYPE" ;;
 esac
 
+# Get full path for compiler file (not relative path)
 function realpath {
-    echo $(cd $(dirname $1); pwd)/$(basename $1);
+  echo $(cd $(dirname $1); pwd)/$(basename $1);
 }
 amxxpc=$(realpath ${amxxpc})
 
-# Create dir
+# Create directory for plugins
 rm -rf ${OUTPUT_DIR} && mkdir ${OUTPUT_DIR}
 mkdir -p ${OUTPUT_DIR}/addons/amxmodx/plugins/
 
+# Copy source files to compiler directory
 scripting_dir=${OUTPUT_DIR}/addons/amxmodx/scripting
 cp -rp cstrike/addons/amxmodx/*    ${OUTPUT_DIR}/addons/amxmodx/
 cp -rp cstrike/addons/amxmodx/scripting/    ${scripting_dir}
 # cp -rp extra/*           ${OUTPUT_DIR}
 
-# Compile
 cd ${scripting_dir}
 echo " > Current dir=${scripting_dir}"
 echo " > amxxpc path=${amxxpc}"
 
+# Compile plugins. Find any *.sma file and provide them to the compiler
 find * -name "*.sma" \
-    -exec echo -e "\n\n> Compile {} <" \;\
-    -exec ${amxxpc} {} \
-        -iinclude \
-        -o../plugins/{} \
-    \;
+  -exec echo -e "\n\n> Compile {} <" \;\
+  -exec ${amxxpc} {} \
+    -iinclude \
+    -o../plugins/{} \
+  \;
 
-# Pack to release
+# Pack into one bundle for better release experience
 cd ../../../
 tar -czpf ${RELEASE_NAME}.tar.gz *
